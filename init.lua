256d1614900bed302750abef03dccc8a1ef00d5c
wyzwin <wyz2016zxc@outlook.com>
2023-07-11 11:51:01 +0800

    add iswindows and islinux

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
+vim.cmd([[
+    if(has("win32") || has("win64") || has("win95") || has("win16"))
+        let g:iswindows = 1
+    else
+         let g:islinux = 1
+    endif
+]])
+
+-- if vim.g.iswindows then
+-- print("windows")
+-- end
+-- if  vim.g.islinux then
+--   print("linux")
+-- end
+
 require('opt')
 require('keymap')
 require('gui')
 require('lazynvim')
+
