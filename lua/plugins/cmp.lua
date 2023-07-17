3fd9601ca70eaf8cec00f883dadc5dbd7f50f306
wyzwin <wyz2016zxc@outlook.com>
2023-07-17 08:24:32 +0800

    cmp seems works

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,60 +17,58 @@ return{
       }
     },
   },
-  opts = function ()
-    vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      return {
-        -- completion = {
-      
-        --   completeopt = "menu,menuone,noinsert",
-        -- },
-        snippet = {
-          expand = function(args)
-            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-          end,
+  config = function ()
+    local cmp = require("cmp")
+    cmp.setup{
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
+      snippet = {
+        expand = function(args)
+          vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end,
+        ['<S-Tab>'] = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end,
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = 'ultisnips' }, -- For ultisnips users.
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        {
+            name = 'spell',
+            option = {
+                keep_all_entries = false,
+                enable_in_context = function()
+                    return true
+                end,
+            },
         },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-          ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              fallback()
-            end
-          end,
-          ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              fallback()
-            end
-          end,
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        }),
-        sources = cmp.config.sources({
-          { name = 'ultisnips' }, -- For ultisnips users.
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          {
-              name = 'spell',
-              option = {
-                  keep_all_entries = true,
-                  enable_in_context = function()
-                      return true
-                  end,
-              },
-          },
-          { name = "path" },
-        }),
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
+        { name = "path" },
+        { name = "nvim_lua" },
+      }),
+      experimental = {
+        ghost_text = {
+          hl_group = "CmpGhostText",
         },
       }
+    }
   end
 }
-
