9dfe41338a713172da2473fbc5d02bad9e95026d
wyzwin <wyz2016zxc@outlook.com>
2023-07-27 21:17:41 +0800

    add startuptime and some autocmds

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -165,4 +165,11 @@ return {
       }, { prefix = "<leader>" })
     end,
   },
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
 }
