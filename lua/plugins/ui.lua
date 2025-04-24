261b5543b5e716af5e1b775a4663f0773a6a3b97
arch14 <wyz2016zxc@outlook.com>
2025-04-24 20:02:33 +0800

    add github theme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -43,13 +43,13 @@ return {
     config = function()
       require("nightfox").setup({
         options = {
-          dim_inactive = true, -- Non focused panes set to alternative background
-          module_default = true, -- Default enable value for modules
+          dim_inactive = true,     -- Non focused panes set to alternative background
+          module_default = true,   -- Default enable value for modules
           colorblind = {
-            enable = true, -- Enable colorblind support
+            enable = true,         -- Enable colorblind support
             simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
           },
-          styles = { -- Style to be applied to different syntax groups
+          styles = {               -- Style to be applied to different syntax groups
             -- bold
             -- underline
             -- undercurl  curly underline
@@ -138,7 +138,7 @@ return {
             -- { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
             {
               "filename",
-              file_status = true, -- Displays file status (readonly status, modified status)
+              file_status = true,    -- Displays file status (readonly status, modified status)
               newfile_status = true, -- Display new file status (new file means no write after created)
               path = 1,
               symbols = {
@@ -176,9 +176,9 @@ return {
             { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
             {
               "filename",
-              file_status = true, -- Displays file status (readonly status, modified status)
+              file_status = true,    -- Displays file status (readonly status, modified status)
               newfile_status = true, -- Display new file status (new file means no write after created)
-              path = 1, -- 0: Just the filename
+              path = 1,              -- 0: Just the filename
               -- 1: Relative path
               -- 2: Absolute path
               -- 3: Absolute path, with tilde as the home directory
@@ -232,7 +232,7 @@ return {
             {
               "tabs",
               max_length = vim.o.columns / 3, -- Maximum width of tabs component.
-              mode = 1, -- 0: Shows tab_nr
+              mode = 1,                       -- 0: Shows tab_nr
               -- 1: Shows tab_name
               -- 2: Shows tab_nr + tab_name
             },
@@ -338,7 +338,7 @@ return {
           },
           style = {
             { bg = vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("cursorline")), "bg", "gui") },
-            { bg = "", fg = "" },
+            { bg = "",                                                                         fg = "" },
           },
         },
       })
@@ -455,7 +455,7 @@ return {
       "hiphish/rainbow-delimiters.nvim",
     },
     keys = {
-      { "<leader>ui", "<cmd>IBLToggle<cr>", desc = "toggle indent" },
+      { "<leader>ui", "<cmd>IBLToggle<cr>",      desc = "toggle indent" },
       { "<leader>uf", "<cmd>IBLToggleScope<cr>", desc = "toggle scope" },
     },
     main = "ibl",
@@ -525,6 +525,14 @@ return {
     end,
   },
   {
+    'wsdjeg/scrollbar.vim',
+    enabled = false,
+    event = "BufRead",
+    config = function()
+      require('scrollbar').setup()
+    end,
+  },
+  {
     "gorbit99/codewindow.nvim",
     enabled = false,
     keys = {
@@ -547,14 +555,14 @@ return {
       local codewindow = require("codewindow")
       codewindow.setup({
         exclude_filetypes = { "help" }, -- Choose certain filetypes to not show minimap on
-        minimap_width = 5, -- The width of the text part of the minimap
-        use_lsp = true, -- Use the builtin LSP to show errors and warnings
-        use_treesitter = false, -- Use nvim-treesitter to highlight the code
-        use_git = true, -- Show small dots to indicate git additions and deletions
-        width_multiplier = 8, -- How many characters one dot represents
-        z_index = 1, -- The z-index the floating window will be on
-        show_cursor = true, -- Show the cursor position in the minimap
-        window_border = "single", -- The border style of the floating window (accepts all usual options)
+        minimap_width = 5,              -- The width of the text part of the minimap
+        use_lsp = true,                 -- Use the builtin LSP to show errors and warnings
+        use_treesitter = false,         -- Use nvim-treesitter to highlight the code
+        use_git = true,                 -- Show small dots to indicate git additions and deletions
+        width_multiplier = 8,           -- How many characters one dot represents
+        z_index = 1,                    -- The z-index the floating window will be on
+        show_cursor = true,             -- Show the cursor position in the minimap
+        window_border = "single",       -- The border style of the floating window (accepts all usual options)
       })
     end,
   },
@@ -566,33 +574,33 @@ return {
     -- Optional
     keys = {
       -- Global Minimap Controls
-      { "<leader>nm", "<cmd>Neominimap toggle<cr>", desc = "Toggle global minimap" },
-      { "<leader>no", "<cmd>Neominimap on<cr>", desc = "Enable global minimap" },
-      { "<leader>nc", "<cmd>Neominimap off<cr>", desc = "Disable global minimap" },
-      { "<leader>nr", "<cmd>Neominimap refresh<cr>", desc = "Refresh global minimap" },
+      { "<leader>nm",  "<cmd>Neominimap toggle<cr>",      desc = "Toggle global minimap" },
+      { "<leader>no",  "<cmd>Neominimap on<cr>",          desc = "Enable global minimap" },
+      { "<leader>nc",  "<cmd>Neominimap off<cr>",         desc = "Disable global minimap" },
+      { "<leader>nr",  "<cmd>Neominimap refresh<cr>",     desc = "Refresh global minimap" },
 
       -- Window-Specific Minimap Controls
-      { "<leader>nwt", "<cmd>Neominimap winToggle<cr>", desc = "Toggle minimap for current window" },
-      { "<leader>nwr", "<cmd>Neominimap winRefresh<cr>", desc = "Refresh minimap for current window" },
-      { "<leader>nwo", "<cmd>Neominimap winOn<cr>", desc = "Enable minimap for current window" },
-      { "<leader>nwc", "<cmd>Neominimap winOff<cr>", desc = "Disable minimap for current window" },
+      { "<leader>nwt", "<cmd>Neominimap winToggle<cr>",   desc = "Toggle minimap for current window" },
+      { "<leader>nwr", "<cmd>Neominimap winRefresh<cr>",  desc = "Refresh minimap for current window" },
+      { "<leader>nwo", "<cmd>Neominimap winOn<cr>",       desc = "Enable minimap for current window" },
+      { "<leader>nwc", "<cmd>Neominimap winOff<cr>",      desc = "Disable minimap for current window" },
 
       -- Tab-Specific Minimap Controls
-      { "<leader>ntt", "<cmd>Neominimap tabToggle<cr>", desc = "Toggle minimap for current tab" },
-      { "<leader>ntr", "<cmd>Neominimap tabRefresh<cr>", desc = "Refresh minimap for current tab" },
-      { "<leader>nto", "<cmd>Neominimap tabOn<cr>", desc = "Enable minimap for current tab" },
-      { "<leader>ntc", "<cmd>Neominimap tabOff<cr>", desc = "Disable minimap for current tab" },
+      { "<leader>ntt", "<cmd>Neominimap tabToggle<cr>",   desc = "Toggle minimap for current tab" },
+      { "<leader>ntr", "<cmd>Neominimap tabRefresh<cr>",  desc = "Refresh minimap for current tab" },
+      { "<leader>nto", "<cmd>Neominimap tabOn<cr>",       desc = "Enable minimap for current tab" },
+      { "<leader>ntc", "<cmd>Neominimap tabOff<cr>",      desc = "Disable minimap for current tab" },
 
       -- Buffer-Specific Minimap Controls
-      { "<leader>nbt", "<cmd>Neominimap bufToggle<cr>", desc = "Toggle minimap for current buffer" },
-      { "<leader>nbr", "<cmd>Neominimap bufRefresh<cr>", desc = "Refresh minimap for current buffer" },
-      { "<leader>nbo", "<cmd>Neominimap bufOn<cr>", desc = "Enable minimap for current buffer" },
-      { "<leader>nbc", "<cmd>Neominimap bufOff<cr>", desc = "Disable minimap for current buffer" },
+      { "<leader>nbt", "<cmd>Neominimap bufToggle<cr>",   desc = "Toggle minimap for current buffer" },
+      { "<leader>nbr", "<cmd>Neominimap bufRefresh<cr>",  desc = "Refresh minimap for current buffer" },
+      { "<leader>nbo", "<cmd>Neominimap bufOn<cr>",       desc = "Enable minimap for current buffer" },
+      { "<leader>nbc", "<cmd>Neominimap bufOff<cr>",      desc = "Disable minimap for current buffer" },
 
       ---Focus Controls
-      { "<leader>nf", "<cmd>Neominimap focus<cr>", desc = "Focus on minimap" },
-      { "<leader>nu", "<cmd>Neominimap unfocus<cr>", desc = "Unfocus minimap" },
-      { "<leader>ns", "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
+      { "<leader>nf",  "<cmd>Neominimap focus<cr>",       desc = "Focus on minimap" },
+      { "<leader>nu",  "<cmd>Neominimap unfocus<cr>",     desc = "Unfocus minimap" },
+      { "<leader>ns",  "<cmd>Neominimap toggleFocus<cr>", desc = "Switch focus on minimap" },
     },
     init = function()
       vim.g.neominimap = {
@@ -648,7 +656,7 @@ return {
             if basename ~= "config" and basename ~= "tmp" and basename ~= "blog" then
               -- if basename ~= "tmp" then
               local button =
-                dashboard.button(tostring(i), "● " .. basename, "<cmd>PLoad " .. basename .. "<cr>")
+                  dashboard.button(tostring(i), "● " .. basename, "<cmd>PLoad " .. basename .. "<cr>")
               table.insert(group.val, button)
               i = i + 1
               -- end
@@ -767,6 +775,7 @@ return {
       -- require("everforest").load()
     end,
   },
+{ 'projekt0n/github-nvim-theme', name = 'github-theme' },
   {
     "rose-pine/neovim",
     -- cmd = "colorscheme",
@@ -774,14 +783,14 @@ return {
     -- name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        variant = "dawn", -- auto, main, moon, or dawn
+        variant = "dawn",      -- auto, main, moon, or dawn
         dark_variant = "main", -- main, moon, or dawn
         dim_inactive_windows = true,
         extend_background_behind_borders = true,
         enable = {
           terminal = false,
           legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
+          migrations = true,        -- Handle deprecated options automatically
         },
         styles = {
           bold = true,
