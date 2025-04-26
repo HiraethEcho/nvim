795b288f431336f7134c09640e90628d3f8bc2a2
arch14 <wyz2016zxc@outlook.com>
2025-04-26 20:25:23 +0800

    rm some keys

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -167,10 +167,9 @@ return {
       { "<leader>gl", function() Snacks.picker.git_log() end,                                 desc = "Git Log", },
       { "<leader>gL", function() Snacks.picker.git_log_line() end,                            desc = "Git Log Line", },
       { "<leader>gs", function() Snacks.picker.git_status() end,                              desc = "Git Status", },
-      { "<leader>gS", function() Snacks.picker.git_stash() end,                               desc = "Git Stash", },
+      -- { "<leader>gS", function() Snacks.picker.git_stash() end,                               desc = "Git Stash", },
       { "<leader>gd", function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)", },
       { "<leader>gf", function() Snacks.picker.git_log_file() end,                            desc = "Git Log File", },
-      -- Grep
       -- search
       { '<leader>"',  function() Snacks.picker.registers() end,                               desc = "Registers", },
       { "<leader>s/", function() Snacks.picker.search_history() end,                          desc = "Search History", },
