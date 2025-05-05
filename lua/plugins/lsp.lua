d2457dd30de167a029659f3b840c67dba6bad44d
arch14 <wyz2016zxc@outlook.com>
2025-05-05 18:37:45 +0800

    try incline

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -244,6 +244,56 @@ return {
       },
     },
   },
+  {
+    "b0o/incline.nvim",
+    -- enabled = false,
+    event = "LspAttach",
+
+    config = function()
+      local helpers = require("incline.helpers")
+      local navic = require("nvim-navic")
+      local devicons = require("nvim-web-devicons")
+      require("incline").setup({
+        window = {
+          padding = 0,
+          margin = { horizontal = 0, vertical = 0 },
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if filename == "" then
+            filename = "[No Name]"
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+          local function get_diagnostic_label()
+            local icons = {
+              error = " ",
+              warn = " ",
+              info = " ",
+              hint = "󰌵 ",
+            }
+            local label = {}
+
+            for severity, icon in pairs(icons) do
+              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+              if n > 0 then
+                table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+              end
+            end
+            if #label > 0 then
+              table.insert(label, { "┊ " })
+            end
+            return label
+          end
+
+          return {
+            { filename },
+            { get_diagnostic_label() },
+          }
+        end,
+      })
+    end,
+  },
   -- disabled
   {
     "bassamsdata/namu.nvim",
