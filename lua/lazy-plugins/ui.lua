3ad72cef75d2f3e5c28c44558df5105d8785371f
arch14 <wyz2016zxc@outlook.com>
2025-08-01 00:49:05 +0800

    backup at 25/08/01-00:49:05

--- lua/lazy-plugins/ui.lua
+++ lua/lazy-plugins/ui.lua
@@ -1,17 +1,36 @@
 vim.g.loaded_matchparen = 1 -- "utilyre/sentiment.nvim",
 return {
   {
-    "norcalli/nvim-colorizer.lua",
-    cmd = "ColorizerToggle",
-    config = function()
-      require("colorizer").setup()
-    end,
+    "catgoose/nvim-colorizer.lua",
+    cmd = { "ColorizerToggle", "ColorizerAttachToBuffer" },
+    opts = { -- set to setup table
+      user_default_options = {
+        names = true,
+        RGB = true, -- #RGB hex codes
+        RGBA = true, -- #RGBA hex codes
+        RRGGBB = true, -- #RRGGBB hex codes #ffffff
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        AARRGGBB = true, -- 0xAARRGGBB hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS *features*:
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+        tailwind = true, -- Enable tailwind colors
+        xterm = true, -- Enable xterm 256-color codes (#xNN, \e[38;5;NNNm)
+        -- Highlighting mode.  'background'|'foreground'|'virtualtext'
+        mode = "background", -- Set the display mode
+        virtualtext = "■",
+        -- Display virtualtext inline with color.  boolean|'before'|'after'.  True sets to 'after'
+        virtualtext_inline = true,
+        virtualtext_mode = "foreground",
+        always_update = true,
+      },
+    },
   },
+  { "brenoprata10/nvim-highlight-colors", enabled = false, opts = { enable_ansi = true } },
+  -- { "norcalli/nvim-colorizer.lua", cmd = "ColorizerToggle", opts = true },
   { "nvzone/volt", lazy = true },
-  {
-    "nvzone/minty",
-    cmd = { "Shades", "Huefy" },
-  },
+  { "nvzone/minty", cmd = { "Shades", "Huefy" } },
   {
     "utilyre/sentiment.nvim",
     version = "*",
@@ -24,13 +43,7 @@ return {
       }, -- config
     },
   },
-  {
-    "OXY2DEV/helpview.nvim",
-    ft = "help",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
+  { "OXY2DEV/helpview.nvim", ft = "help", dependencies = { "nvim-treesitter/nvim-treesitter" } },
   -- disable
   {
     "rcarriga/nvim-notify",
@@ -53,7 +66,6 @@ return {
   },
   {
     "gen740/SmoothCursor.nvim",
-    lazy = false,
     enabled = false,
     config = function()
       require("smoothcursor").setup({
@@ -65,15 +77,13 @@ return {
     "stevearc/dressing.nvim",
     enabled = false,
     event = "BufReadPost",
-    opts = {},
+    opts = true,
   },
   {
     "xiyaowong/nvim-transparent",
     enabled = false,
     cmd = "TransparentEnable",
     -- lazy=false,
-    config = function()
-      require("transparent").setup({})
-    end,
+    opts = true,
   },
 }
